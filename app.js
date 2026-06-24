af0051201444070f6e575c8d24dae9c5baace16b
Senossi <abdelrahman.sanosi@paysky.io>
2026-06-24 12:19:42 +0000

    Initial DevOps App

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req,res)=>{
+
+    res.send('DevOps Project Running')
+
+})
+
+app.listen(3000, ()=>{
+
+    console.log('Server Started')
+
+})
+ 
